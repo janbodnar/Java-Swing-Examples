976db95b22f6faecc23929df3b325bc105c36c75
Jan Bodnar <jan.bodnar@gmail.com>
2018-03-05 18:19:37 +0100

    Create TooltipEx.java

---
+++ firstprograms/TooltipEx.java
@@ -0,0 +1,59 @@
+package com.zetcode;
+
+import java.awt.EventQueue;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TooltipEx extends JFrame {
+
+    public TooltipEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        JButton btn = new JButton("Button");
+        btn.setToolTipText("A button component");
+
+        createLayout(btn);
+
+        setTitle("Tooltip");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void createLayout(JComponent... arg) {
+
+        JPanel pane = (JPanel) getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        pane.setToolTipText("Content pane");
+
+        gl.setAutoCreateContainerGaps(true);
+
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+                .addGap(200)
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+                .addGap(120)
+        );
+
+        pack();
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            TooltipEx ex = new TooltipEx();
+            ex.setVisible(true);
+        });
+    }
+}
