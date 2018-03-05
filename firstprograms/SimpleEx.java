d3136b5617e16deab9cc4b9de7c03071d7d8414a
Jan Bodnar <jan.bodnar@gmail.com>
2018-03-05 18:20:50 +0100

    Create SimpleEx.java

---
+++ firstprograms/SimpleEx.java
@@ -0,0 +1,28 @@
+package com.zetcode;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class SimpleEx extends JFrame {
+
+    public SimpleEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        setTitle("Simple example");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            SimpleEx ex = new SimpleEx();
+            ex.setVisible(true);
+        });
+    }
+}
