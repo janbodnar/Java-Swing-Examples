80db3c89abaffa2c3b17decfca1c8def7814c27e
Jan Bodnar <jan.bodnar@gmail.com>
2018-03-06 12:29:20 +0100

    Create SimpleMenuEx.java

---
+++ menus/SimpleMenuEx.java
@@ -0,0 +1,58 @@
+package com.zetcode;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class SimpleMenuEx extends JFrame {
+
+    public SimpleMenuEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        createMenuBar();
+
+        setTitle("Simple menu");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void createMenuBar() {
+
+        JMenuBar menubar = new JMenuBar();
+        ImageIcon icon = new ImageIcon("src/main/resources/exit.png");
+
+        JMenu file = new JMenu("File");
+        file.setMnemonic(KeyEvent.VK_F);
+
+        JMenuItem eMenuItem = new JMenuItem("Exit", icon);
+        eMenuItem.setMnemonic(KeyEvent.VK_E);
+        eMenuItem.setToolTipText("Exit application");
+        eMenuItem.addActionListener((ActionEvent event) -> {
+            System.exit(0);
+        });
+
+        file.add(eMenuItem);
+
+        menubar.add(file);
+
+        setJMenuBar(menubar);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            SimpleMenuEx ex = new SimpleMenuEx();
+            ex.setVisible(true);
+        });
+    }
+}
