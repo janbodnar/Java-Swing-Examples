13f520f6c41efaff1408415ca9ece8f04aaf7e51
Jan Bodnar <jan.bodnar@gmail.com>
2018-03-06 19:18:16 +0100

    Create ToolbarEx.java

---
+++ menus/ToolbarEx.java
@@ -0,0 +1,62 @@
+package com.zetcode;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JToolBar;
+
+public class ToolbarEx extends JFrame {
+
+    public ToolbarEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        createMenuBar();
+        createToolBar();
+
+        setTitle("Simple toolbar");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void createMenuBar() {
+
+        JMenuBar menubar = new JMenuBar();
+        JMenu file = new JMenu("File");
+        menubar.add(file);
+        setJMenuBar(menubar);
+    }
+
+    private void createToolBar() {
+
+        JToolBar toolbar = new JToolBar();
+
+        ImageIcon icon = new ImageIcon("src/main/resources/exit2.png");
+
+        JButton exitButton = new JButton(icon);
+        toolbar.add(exitButton);
+
+        exitButton.addActionListener((ActionEvent event) -> {
+            System.exit(0);
+        });
+
+        add(toolbar, BorderLayout.NORTH);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            ToolbarEx ex = new ToolbarEx();
+            ex.setVisible(true);
+        });
+    }
+}
