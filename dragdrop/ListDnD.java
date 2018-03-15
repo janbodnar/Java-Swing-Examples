420edf2fc414e09a0228efe48872d3e248276760
Jan Bodnar <jan.bodnar@gmail.com>
2018-03-15 13:28:56 +0100

    Create ListDnD.java

---
+++ dragdrop/ListDnD.java
@@ -0,0 +1,132 @@
+package com.zetcode;
+
+import javax.swing.DefaultListModel;
+import javax.swing.DropMode;
+import javax.swing.GroupLayout;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.TransferHandler;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+
+/*
+Java Swing tutorial
+JList drag and drop example
+
+Author: Jan Bodnar
+Website: http://zetcode.com
+ */
+
+public class ListDnD extends JFrame {
+
+    private JTextField field;
+    private DefaultListModel model;
+
+    public ListDnD() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setPreferredSize(new Dimension(180, 150));
+
+        model = new DefaultListModel();
+        JList list = new JList(model);
+
+        list.setDropMode(DropMode.INSERT);
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.setTransferHandler(new ListHandler());
+
+        field = new JTextField(15);
+        field.setDragEnabled(true);
+
+        scrollPane.getViewport().add(list);
+
+        createLayout(field, scrollPane);
+
+        setTitle("ListDrop");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private class ListHandler extends TransferHandler {
+
+        public boolean canImport(TransferSupport support) {
+
+            if (!support.isDrop()) {
+                return false;
+            }
+
+            return support.isDataFlavorSupported(DataFlavor.stringFlavor);
+        }
+
+        public boolean importData(TransferSupport support) {
+
+            if (!canImport(support)) {
+                return false;
+            }
+
+            Transferable transferable = support.getTransferable();
+            String line;
+
+            try {
+                line = (String) transferable.getTransferData(DataFlavor.stringFlavor);
+            } catch (Exception e) {
+                return false;
+            }
+
+            JList.DropLocation dl = (JList.DropLocation) support.getDropLocation();
+            int index = dl.getIndex();
+
+            String[] data = line.split("[,\\s]");
+
+            for (String item : data) {
+                if (!item.isEmpty())
+                    model.add(index++, item.trim());
+            }
+
+            return true;
+        }
+    }
+
+    private void createLayout(JComponent... arg) {
+
+        Container pane = getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+        gl.setAutoCreateGaps(true);
+
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+                .addComponent(arg[1])
+        );
+
+        gl.setVerticalGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                .addComponent(arg[0])
+                .addComponent(arg[1])
+        );
+
+        pack();
+    }
+
+    public static void main(String[] args) {
+    
+        EventQueue.invokeLater(() -> {
+
+            ListDnD ex = new ListDnD();
+            ex.setVisible(true);
+        });
+    }
+}
