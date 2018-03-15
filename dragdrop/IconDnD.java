8e30e65171c56342593ae8907939bf873e5533e9
Jan Bodnar <jan.bodnar@gmail.com>
2018-03-15 13:07:17 +0100

    Create IconDnD.java

---
+++ dragdrop/IconDnD.java
@@ -0,0 +1,114 @@
+package com.zetcode;
+
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.TransferHandler;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/*
+Java Swing tutorial
+Drag and drop icons
+
+Author: Jan Bodnar
+Website: http://zetcode.com
+ */
+
+public class IconDnD extends JFrame {
+
+
+    public IconDnD() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        ImageIcon icon1 = new ImageIcon("src/main/resources/sad.png");
+        ImageIcon icon2 = new ImageIcon("src/main/resources/plain.png");
+        ImageIcon icon3 = new ImageIcon("src/main/resources/smile.png");
+
+        JLabel label1 = new JLabel(icon1, JLabel.CENTER);
+        JLabel label2 = new JLabel(icon2, JLabel.CENTER);
+        JLabel label3 = new JLabel(icon3, JLabel.CENTER);
+
+        MouseListener listener = new DragMouseAdapter();
+        label1.addMouseListener(listener);
+        label2.addMouseListener(listener);
+        label3.addMouseListener(listener);
+
+        JButton button = new JButton(icon2);
+        button.setFocusable(false);
+
+        label1.setTransferHandler(new TransferHandler("icon"));
+        label2.setTransferHandler(new TransferHandler("icon"));
+        label3.setTransferHandler(new TransferHandler("icon"));
+        button.setTransferHandler(new TransferHandler("icon"));
+
+        createLayout(label1, label2, label3, button);
+
+        setTitle("Icon Drag & Drop");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private class DragMouseAdapter extends MouseAdapter {
+
+        public void mousePressed(MouseEvent e) {
+
+            JComponent c = (JComponent) e.getSource();
+            TransferHandler handler = c.getTransferHandler();
+            handler.exportAsDrag(c, e, TransferHandler.COPY);
+        }
+    }
+
+    private void createLayout(JComponent... arg) {
+
+        Container pane = getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+        gl.setAutoCreateGaps(true);
+
+        gl.setHorizontalGroup(gl.createParallelGroup(GroupLayout.Alignment.CENTER)
+                .addGroup(gl.createSequentialGroup()
+                        .addComponent(arg[0])
+                        .addGap(30)
+                        .addComponent(arg[1])
+                        .addGap(30)
+                        .addComponent(arg[2])
+                )
+                .addComponent(arg[3], GroupLayout.DEFAULT_SIZE,
+                        GroupLayout.DEFAULT_SIZE, Integer.MAX_VALUE)
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addGroup(gl.createParallelGroup()
+                        .addComponent(arg[0])
+                        .addComponent(arg[1])
+                        .addComponent(arg[2]))
+                .addGap(30)
+                .addComponent(arg[3])
+        );
+
+        pack();
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            IconDnD ex = new IconDnD();
+            ex.setVisible(true);
+        });
+    }
+}
