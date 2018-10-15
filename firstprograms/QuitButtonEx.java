77b4dff162b1db61301f1894e7b67a2f3fae6b50
Jan Bodnar <jan.bodnar@gmail.com>
2018-10-15 22:55:49 +0200

    Update QuitButtonEx.java

--- firstprograms/QuitButtonEx.java
+++ firstprograms/QuitButtonEx.java
@@ -1,12 +1,11 @@
 package com.zetcode;
 
-import java.awt.Container;
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
 
 public class QuitButtonEx extends JFrame {
 
@@ -17,11 +16,9 @@ public class QuitButtonEx extends JFrame {
 
     private void initUI() {
 
-        JButton quitButton = new JButton("Quit");
+        var quitButton = new JButton("Quit");
 
-        quitButton.addActionListener((ActionEvent event) -> {
-            System.exit(0);
-        });
+        quitButton.addActionListener((event) -> System.exit(0));
 
         createLayout(quitButton);
 
@@ -33,8 +30,8 @@ public class QuitButtonEx extends JFrame {
 
     private void createLayout(JComponent... arg) {
 
-        Container pane = getContentPane();
-        GroupLayout gl = new GroupLayout(pane);
+        var pane = getContentPane();
+        var gl = new GroupLayout(pane);
         pane.setLayout(gl);
 
         gl.setAutoCreateContainerGaps(true);
@@ -51,7 +48,7 @@ public class QuitButtonEx extends JFrame {
     public static void main(String[] args) {
 
         EventQueue.invokeLater(() -> {
-            QuitButtonEx ex = new QuitButtonEx();
+            var ex = new QuitButtonEx();
             ex.setVisible(true);
         });
     }
