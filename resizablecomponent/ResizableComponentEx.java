a4876c0e27d97a1563b133145cce61349c3e4c32
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-01 19:14:23 +0100

    Create ResizableComponentEx.java

---
+++ resizablecomponent/ResizableComponentEx.java
@@ -0,0 +1,54 @@
+package com.zetcode;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ResizableComponentEx extends JFrame {
+
+    private Resizable res;
+
+    public ResizableComponentEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        var pnl = new JPanel(null);
+        add(pnl);
+
+        var area = new JPanel();
+        area.setBackground(Color.white);
+
+        res = new Resizable(area);
+        res.setBounds(50, 50, 200, 150);
+        pnl.add(res);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+
+                requestFocus();
+                res.repaint();
+            }
+        });
+
+        setSize(350, 300);
+        setTitle("Resizable component");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            var ex = new ResizableComponentEx();
+            ex.setVisible(true);
+        });
+    }
+}
