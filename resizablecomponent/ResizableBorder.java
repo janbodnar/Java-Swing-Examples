fd833add3f5972cb92b7b57561c718e673d22cb8
Jan Bodnar <jan.bodnar@gmail.com>
2020-06-28 22:51:25 +0200

    Update ResizableBorder.java

--- resizablecomponent/ResizableBorder.java
+++ resizablecomponent/ResizableBorder.java
@@ -1,5 +1,7 @@
 package com.zetcode;
 
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Cursor;
@@ -7,21 +9,19 @@ import java.awt.Graphics;
 import java.awt.Insets;
 import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
-import javax.swing.SwingConstants;
-import javax.swing.border.Border;
 
 public class ResizableBorder implements Border {
 
     private int dist = 8;
 
-    int locations[] = {
+    int[] locations = {
             SwingConstants.NORTH, SwingConstants.SOUTH, SwingConstants.WEST,
             SwingConstants.EAST, SwingConstants.NORTH_WEST,
             SwingConstants.NORTH_EAST, SwingConstants.SOUTH_WEST,
             SwingConstants.SOUTH_EAST
     };
 
-    int cursors[] = {
+    int[] cursors = {
             Cursor.N_RESIZE_CURSOR, Cursor.S_RESIZE_CURSOR, Cursor.W_RESIZE_CURSOR,
             Cursor.E_RESIZE_CURSOR, Cursor.NW_RESIZE_CURSOR, Cursor.NE_RESIZE_CURSOR,
             Cursor.SW_RESIZE_CURSOR, Cursor.SE_RESIZE_CURSOR
@@ -50,47 +50,34 @@ public class ResizableBorder implements Border {
 
         if (component.hasFocus()) {
 
-            for (int i = 0; i < locations.length; i++) {
+            for (int location : locations) {
 
-                var rect = getRectangle(x, y, w, h, locations[i]);
+                var rect = getRectangle(x, y, w, h, location);
 
-                g.setColor(Color.WHITE);
-                g.fillRect(rect.x, rect.y, rect.width - 1, rect.height - 1);
-                g.setColor(Color.BLACK);
-                g.drawRect(rect.x, rect.y, rect.width - 1, rect.height - 1);
+                if (rect != null) {
+
+                    g.setColor(Color.WHITE);
+                    g.fillRect(rect.x, rect.y, rect.width - 1, rect.height - 1);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(rect.x, rect.y, rect.width - 1, rect.height - 1);
+                }
             }
         }
     }
 
     private Rectangle getRectangle(int x, int y, int w, int h, int location) {
 
-        switch (location) {
-
-            case SwingConstants.NORTH:
-                return new Rectangle(x + w / 2 - dist / 2, y, dist, dist);
-
-            case SwingConstants.SOUTH:
-                return new Rectangle(x + w / 2 - dist / 2, y + h - dist, dist, dist);
-
-            case SwingConstants.WEST:
-                return new Rectangle(x, y + h / 2 - dist / 2, dist, dist);
-
-            case SwingConstants.EAST:
-                return new Rectangle(x + w - dist, y + h / 2 - dist / 2, dist, dist);
-
-            case SwingConstants.NORTH_WEST:
-                return new Rectangle(x, y, dist, dist);
-
-            case SwingConstants.NORTH_EAST:
-                return new Rectangle(x + w - dist, y, dist, dist);
-
-            case SwingConstants.SOUTH_WEST:
-                return new Rectangle(x, y + h - dist, dist, dist);
-
-            case SwingConstants.SOUTH_EAST:
-                return new Rectangle(x + w - dist, y + h - dist, dist, dist);
-        }
-        return null;
+        return switch (location) {
+            case SwingConstants.NORTH -> new Rectangle(x + w / 2 - dist / 2, y, dist, dist);
+            case SwingConstants.SOUTH -> new Rectangle(x + w / 2 - dist / 2, y + h - dist, dist, dist);
+            case SwingConstants.WEST -> new Rectangle(x, y + h / 2 - dist / 2, dist, dist);
+            case SwingConstants.EAST -> new Rectangle(x + w - dist, y + h / 2 - dist / 2, dist, dist);
+            case SwingConstants.NORTH_WEST -> new Rectangle(x, y, dist, dist);
+            case SwingConstants.NORTH_EAST -> new Rectangle(x + w - dist, y, dist, dist);
+            case SwingConstants.SOUTH_WEST -> new Rectangle(x, y + h - dist, dist, dist);
+            case SwingConstants.SOUTH_EAST -> new Rectangle(x + w - dist, y + h - dist, dist, dist);
+            default -> null;
+        };
     }
 
     public int getCursor(MouseEvent me) {
@@ -103,7 +90,7 @@ public class ResizableBorder implements Border {
 
             var rect = getRectangle(0, 0, w, h, locations[i]);
 
-            if (rect.contains(me.getPoint())) {
+            if (rect != null && rect.contains(me.getPoint())) {
                 return cursors[i];
             }
         }
