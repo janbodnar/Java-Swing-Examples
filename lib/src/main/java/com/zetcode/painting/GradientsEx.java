75d85abba6da6cd78ebb5fe65985364b050dfe63
Arun Prasaad <arunbear@cpan.org>
2023-09-09 15:50:01 +0100

    Fix duplicate classes

--- lib/src/main/java/com/zetcode/painting/GradientsEx.java
+++ lib/src/main/java/com/zetcode/painting/GradientsEx.java
@@ -8,52 +8,53 @@ import java.awt.Graphics2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-class DrawPanel extends JPanel {
 
-    private void doDrawing(Graphics g) {
-
-        var g2d = (Graphics2D) g;
-
-        var gp1 = new GradientPaint(5, 5,
-                Color.red, 20, 20, Color.black, true);
-
-        g2d.setPaint(gp1);
-        g2d.fillRect(20, 20, 300, 40);
-
-        var gp2 = new GradientPaint(5, 25,
-                Color.yellow, 20, 2, Color.black, true);
-
-        g2d.setPaint(gp2);
-        g2d.fillRect(20, 80, 300, 40);
-
-        var gp3 = new GradientPaint(5, 25,
-                Color.green, 2, 2, Color.black, true);
-
-        g2d.setPaint(gp3);
-        g2d.fillRect(20, 140, 300, 40);
-
-        var gp4 = new GradientPaint(25, 25,
-                Color.blue, 15, 25, Color.black, true);
-
-        g2d.setPaint(gp4);
-        g2d.fillRect(20, 200, 300, 40);
-
-        var gp5 = new GradientPaint(0, 0,
-                Color.orange, 0, 20, Color.black, true);
-
-        g2d.setPaint(gp5);
-        g2d.fillRect(20, 260, 300, 40);
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        doDrawing(g);
-    }
-}
 
 public class GradientsEx extends JFrame {
+    static class DrawPanel extends JPanel {
+
+        private void doDrawing(Graphics g) {
+
+            var g2d = (Graphics2D) g;
+
+            var gp1 = new GradientPaint(5, 5,
+                    Color.red, 20, 20, Color.black, true);
+
+            g2d.setPaint(gp1);
+            g2d.fillRect(20, 20, 300, 40);
+
+            var gp2 = new GradientPaint(5, 25,
+                    Color.yellow, 20, 2, Color.black, true);
+
+            g2d.setPaint(gp2);
+            g2d.fillRect(20, 80, 300, 40);
+
+            var gp3 = new GradientPaint(5, 25,
+                    Color.green, 2, 2, Color.black, true);
+
+            g2d.setPaint(gp3);
+            g2d.fillRect(20, 140, 300, 40);
+
+            var gp4 = new GradientPaint(25, 25,
+                    Color.blue, 15, 25, Color.black, true);
+
+            g2d.setPaint(gp4);
+            g2d.fillRect(20, 200, 300, 40);
+
+            var gp5 = new GradientPaint(0, 0,
+                    Color.orange, 0, 20, Color.black, true);
+
+            g2d.setPaint(gp5);
+            g2d.fillRect(20, 260, 300, 40);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            doDrawing(g);
+        }
+    }
 
     public GradientsEx() {
 
