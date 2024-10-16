9f7e698241bfef8cdd2c9aff6a922e82dd9d0896
Jan Bodnar <jan.bodnar@gmail.com>
2024-10-16 12:16:29 +0200

    Update LabelEx.java

--- components/LabelEx.java
+++ components/LabelEx.java
@@ -17,37 +17,38 @@ public class LabelEx extends JFrame {
 
     private void initUI() {
 
-        var lyrics =  "<html>It's way too late to think of<br>" +
-                "Someone I would call now<br>" +
-                "And neon signs got tired<br>" +
-                "Red eye flights help the stars out<br>" +
-                "I'm safe in a corner<br>" +
-                "Just hours before me<br>" +
-                "<br>" +
-                "I'm waking with the roaches<br>" +
-                "The world has surrendered<br>" +
-                "I'm dating ancient ghosts<br>" +
-                "The ones I made friends with<br>" +
-                "The comfort of fireflies<br>" +
-                "Long gone before daylight<br>" +
-                "<br>" +
-                "And if I had one wishful field tonight<br>" +
-                "I'd ask for the sun to never rise<br>" +
-                "If God leant his voice for me to speak<br>" +
-                "I'd say go to bed, world<br>" +
-                "<br>" +
-                "I've always been too late<br>" +
-                "To see what's before me<br>" +
-                "And I know nothing sweeter than<br>" +
-                "Champaign from last New Years<br>" +
-                "Sweet music in my ears<br>" +
-                "And a night full of no fears<br>" +
-                "<br>" +
-                "But if I had one wishful field tonight<br>" +
-                "I'd ask for the sun to never rise<br>" +
-                "If God passed a mic to me to speak<br>" +
-                "I'd say stay in bed, world<br>" +
-                "Sleep in peace</html>";
+        var lyrics =  """
+                <html>It's way too late to think of<br>
+                Someone I would call now<br>
+                And neon signs got tired<br>
+                Red eye flights help the stars out<br>
+                I'm safe in a corner<br>
+                Just hours before me<br>
+                <br>
+                I'm waking with the roaches<br>
+                The world has surrendered<br>
+                I'm dating ancient ghosts<br>
+                The ones I made friends with<br>
+                The comfort of fireflies<br>
+                Long gone before daylight<br>
+                <br>
+                And if I had one wishful field tonight<br>
+                I'd ask for the sun to never rise<br>
+                If God leant his voice for me to speak<br>
+                I'd say go to bed, world<br>
+                <br>
+                I've always been too late<br>
+                To see what's before me<br>
+                And I know nothing sweeter than<br>
+                Champaign from last New Years<br>
+                Sweet music in my ears<br>
+                And a night full of no fears<br>
+                <br>
+                But if I had one wishful field tonight<br>
+                I'd ask for the sun to never rise<br>
+                If God passed a mic to me to speak<br>
+                I'd say stay in bed, world<br>
+                Sleep in peace</html>""";
 
         var label = new JLabel(lyrics);
         label.setFont(new Font("Serif", Font.PLAIN, 14));
